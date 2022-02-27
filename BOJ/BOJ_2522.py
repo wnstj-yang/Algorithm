b2ec9675c8a3f9dec48824cbf54fffa01815f230
wnstj1704 <wnstj1704@naver.com>
2022-02-27 21:23:33 +0900

    Create. 2522 별 찍기 - 12

---
+++ BOJ/BOJ_2522.py
@@ -0,0 +1,11 @@
+# Baekjoon Online Judge - 2522번. 별 찍기 - 12
+
+N = int(input())
+
+for i in range(1, N + 1):
+    print(' ' * (N - i), end='')
+    print('*' * i)
+
+for i in range(N - 1, 0, -1):
+    print(' ' * (N - i), end='')
+    print('*' * i)
