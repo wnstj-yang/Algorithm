1829d369902a8217b6c35eea0f2315f27997430e
wnstj1704 <wnstj1704@naver.com>
2023-02-21 00:15:56 +0900

    fix. 자물쇠와 열쇠 정답 코드

--- 이것이 코딩 테스트다/2. 구현/유형별_자물쇠와 열쇠.py
+++ 이것이 코딩 테스트다/2. 구현/유형별_자물쇠와 열쇠.py
@@ -1,8 +1,12 @@
-def rotate(arr, M):
+# 유형별 구현 A10. 518p - 자물쇠와 열쇠
+# https://school.programmers.co.kr/learn/courses/30/lessons/60059
+
+
+def rotate(key, M):
     temp = [[0] * M for _ in range(M)]
     for i in range(M):
         for j in range(M):
-            temp[j][M - i - 1] = arr[i][j]
+            temp[j][M - i - 1] = key[i][j]
 
     return temp
 
@@ -11,18 +15,17 @@ def check(board, N):
     for i in range(N, N * 2):
         for j in range(N, N * 2):
             if board[i][j] != 1:
-                return True
-    return False
+                return False
+    return True
 
 
 def solution(key, lock):
-    answer = True
     N = len(lock)
     M = len(key)
     board = [[0] * (N * 3) for _ in range(N * 3)]
-    for i in range(N, N * 2):
-        for j in range(N, N * 2):
-            board[i][j] = lock[i - N][j - N]
+    for i in range(N):
+        for j in range(N):
+            board[i + N][j + N] = lock[i][j]
     for _ in range(4):
         key = rotate(key, M)
         for i in range(N * 2):
@@ -38,4 +41,4 @@ def solution(key, lock):
                     for y in range(M):
                         board[i + x][j + y] -= key[x][y]
 
-    return answer
+    return False
