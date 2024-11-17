1612fd22d6e91b721a12636ec68d31b9d703c181
양준서(JoonSeo Yang) <77601720+wnstj-yang@users.noreply.github.com>
2024-11-17 21:56:34 +0900

    [level 2] Title: 숫자의 표현, Time: 6.69 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.py
@@ -0,0 +1,12 @@
+def solution(n):
+    answer = 0
+    for i in range(1, n + 1):
+        cnt = 0
+        for j in range(i, n + 1):
+            cnt += j
+            if cnt == n:
+                answer += 1
+                break
+            if cnt > n:
+                break
+    return answer
