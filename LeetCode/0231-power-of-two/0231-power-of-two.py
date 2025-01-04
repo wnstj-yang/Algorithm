ec05deb2b614b09283655f3ce589d879e112f693
양준서(JoonSeo Yang) <77601720+wnstj-yang@users.noreply.github.com>
2025-01-04 22:22:09 +0900

    Time: 0 ms (100%), Space: 12.4 MB (20.41%) - LeetHub

--- LeetCode/0231-power-of-two/0231-power-of-two.py
+++ LeetCode/0231-power-of-two/0231-power-of-two.py
@@ -1,11 +1,10 @@
 class Solution(object):
     def isPowerOfTwo(self, n):
-        if n == 0: return False
-        while True:
-            if n == 1:
-                break
-            if n % 2 == 1:
-                return False
-            n = n // 2
-        return True
+        # n은 2의 제곱수에 속한다면 비트로 단 하나의 1만 가진다.
+        # n - 1은 n을 제외하고 오른쪽의 비트가 모두 1이다.
+        # 이 둘을 AND연산 했을 때 모두 0이 나온다면 2의 제곱수에 맞는 것이다.
+        result = n & (n - 1)
+        if n > 0 and result == 0:
+            return True
+        return False
         
