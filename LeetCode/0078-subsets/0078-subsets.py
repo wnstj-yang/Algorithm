94198db7ddc7869ee4049b4a03137faaaadf0a13
양준서(JoonSeo Yang) <77601720+wnstj-yang@users.noreply.github.com>
2025-01-12 15:15:50 +0900

    Time: 3 ms (19.53%), Space: 12.5 MB (56.47%) - LeetHub

--- LeetCode/0078-subsets/0078-subsets.py
+++ LeetCode/0078-subsets/0078-subsets.py
@@ -12,4 +12,24 @@ class Solution(object):
                     subset.append(nums[i])
             result.append(subset)
         return result
-        
+
+
+# 직접 모두 순회하는 풀이
+# class Solution(object):
+#     def subsets(self, nums):
+#         result = [[]]
+#         def get_subset(index, cnt, k, num, visited):
+#             if cnt == k:
+#                 result.append(list(num))
+#                 return
+#             for i in range(index, len(nums)):
+#                 if not visited[i]:
+#                     visited[i] = True
+#                     num[cnt] = nums[i]
+#                     get_subset(i + 1, cnt + 1, k, num, visited)
+#                     visited[i] = False
+#         for i in range(1, len(nums) + 1):
+#             num = [0] * i
+#             visited = [False] * len(nums)
+#             get_subset(0, 0, i, num, visited)
+#         return result
