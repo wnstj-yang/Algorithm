fad19a372b797f0b03e4c3a688c67d0b1d27dede
양준서(JoonSeo Yang) <77601720+wnstj-yang@users.noreply.github.com>
2025-01-13 21:31:55 +0900

    Time: 12 ms (60.19%), Space: 25.9 MB (33.73%) - LeetHub

--- LeetCode/0217-contains-duplicate/0217-contains-duplicate.py
+++ LeetCode/0217-contains-duplicate/0217-contains-duplicate.py
@@ -1,7 +1,13 @@
 class Solution(object):
     def containsDuplicate(self, nums):
-        nums.sort()
-        for i in range(len(nums) - 1):
-            if nums[i] == nums[i + 1]:
+        # nums.sort()
+        # for i in range(len(nums) - 1):
+        #     if nums[i] == nums[i + 1]:
+        #         return True
+        # return False
+        nums_set = set()
+        for num in nums:
+            if num in nums_set:
                 return True
+            nums_set.add(num)
         return False
