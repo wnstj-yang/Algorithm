073a6435b3018a9add33aa0af60b6ee83446c5f8
양준서(JoonSeo Yang) <77601720+wnstj-yang@users.noreply.github.com>
2025-01-16 20:12:13 +0900

    Time: 9 ms (60.41%), Space: 13.7 MB (41.15%) - LeetHub

---
+++ LeetCode/0169-majority-element/0169-majority-element.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def majorityElement(self, nums):
+        ans = 0
+        nums_cnt = {}
+        for num in nums:
+            if num not in nums_cnt:
+                nums_cnt[num] = 1
+            else:
+                nums_cnt[num] += 1
+        result = sorted(nums_cnt.items(), key=lambda x:(-x[1]))
+        return result[0][0]
+                
