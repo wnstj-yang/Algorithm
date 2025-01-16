950e3420d477dcf1e61eb79a0e6dede5e905ef19
양준서(JoonSeo Yang) <77601720+wnstj-yang@users.noreply.github.com>
2025-01-16 19:51:08 +0900

    Time: 18 ms (25.82%), Space: 15.6 MB (74.42%) - LeetHub

--- LeetCode/0155-min-stack/0155-min-stack.py
+++ LeetCode/0155-min-stack/0155-min-stack.py
@@ -2,12 +2,17 @@ class MinStack(object):
 
     def __init__(self):
         self.stack = []
+        self.min_stack = [] # 최소값들을 계속 넣으며 변수명 그대로 최소값들의 스택이다.
 
     def push(self, val):
         self.stack.append(val)
+        if not self.min_stack or self.min_stack[-1] >= val:
+            self.min_stack.append(val)
         
 
     def pop(self):
+        if self.stack[-1] == self.min_stack[-1]:
+            self.min_stack.pop()
         self.stack.pop()
         
 
@@ -16,10 +21,7 @@ class MinStack(object):
         
 
     def getMin(self):
-        min_number = (2 ** 31) - 1
-        for num in self.stack:
-            min_number = min(num, min_number)
-        return min_number
+        return self.min_stack[-1]
         
 
 
