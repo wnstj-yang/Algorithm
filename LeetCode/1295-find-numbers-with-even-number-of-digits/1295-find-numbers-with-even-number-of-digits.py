1b64cf40dea1a7708fb0b71ff5417c49f708a75c
양준서(JoonSeo Yang) <77601720+wnstj-yang@users.noreply.github.com>
2025-01-22 21:45:31 +0900

    Time: 6 ms (16.27%), Space: 12.6 MB (18.54%) - LeetHub

--- LeetCode/1295-find-numbers-with-even-number-of-digits/1295-find-numbers-with-even-number-of-digits.py
+++ LeetCode/1295-find-numbers-with-even-number-of-digits/1295-find-numbers-with-even-number-of-digits.py
@@ -2,7 +2,15 @@ class Solution(object):
     def findNumbers(self, nums):
         ans = 0
         for num in nums:
-            if len(str(num)) % 2 == 0:
+            cnt = 0
+            while num != 0:
+                num = num // 10
+                cnt += 1
+            if cnt % 2 == 0:
                 ans += 1
         return ans
+        # for num in nums:
+        #     if len(str(num)) % 2 == 0:
+        #         ans += 1
+        # return ans
         
