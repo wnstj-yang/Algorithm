4d7a5d9c8e359bf7600793c2e8c4eb315da6664a
양준서(JoonSeo Yang) <77601720+wnstj-yang@users.noreply.github.com>
2025-01-24 21:10:15 +0900

    Time: 12 ms (60.24%), Space: 14.1 MB (84.14%) - LeetHub

---
+++ LeetCode/0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.py
@@ -0,0 +1,7 @@
+class Solution(object):
+    def sortedSquares(self, nums):
+        for i in range(len(nums)):
+            nums[i] = nums[i] * nums[i]
+        nums.sort()
+        return nums
+        
