a774fccdd065669d1538216b4eb8b9c661acce79
양준서(JoonSeo Yang) <77601720+wnstj-yang@users.noreply.github.com>
2025-01-31 23:21:52 +0900

    Time: 7 ms (18.14%), Space: 19.9 MB (56.28%) - LeetHub

--- LeetCode/0344-reverse-string/0344-reverse-string.py
+++ LeetCode/0344-reverse-string/0344-reverse-string.py
@@ -4,5 +4,12 @@ class Solution(object):
         :type s: List[str]
         :rtype: None Do not return anything, modify s in-place instead.
         """
-        s[:] = s[::-1]
+        # 역으로 처리한 것에 대한 복사
+        # s[:] = s[::-1]
+        left, right = 0, len(s) - 1
+        while left < right:
+            s[left], s[right] = s[right], s[left]
+            left += 1
+            right -= 1
+            
         
