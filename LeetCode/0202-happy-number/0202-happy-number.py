75d5f92ee4225588df6746ea3d1fd906c33527d3
양준서(JoonSeo Yang) <77601720+wnstj-yang@users.noreply.github.com>
2025-02-01 16:46:47 +0900

    Time: 1 ms (73.39%), Space: 12.5 MB (7.8%) - LeetHub

--- LeetCode/0202-happy-number/0202-happy-number.py
+++ LeetCode/0202-happy-number/0202-happy-number.py
@@ -1,5 +1,8 @@
 class Solution(object):
     def isHappy(self, n):
+        # 규칙이 존재 -> n이 1이나 7이면 True이다.
+        # 그외 한 자리수의 경우에는 False
+        # 지속적으로 한자리 수에서 곱하고 나누면서 과정을 거쳐야하기 때문에 재귀를 활용
         if n == 1 or n == 7:
             return True
         elif n < 10:
