610547cd743ed92ce0e994db32382aad44c19ad8
양준서(JoonSeo Yang) <77601720+wnstj-yang@users.noreply.github.com>
2025-02-16 22:43:42 +0900

    Time: 3 ms (30.64%), Space: 12.3 MB (80.5%) - LeetHub

---
+++ LeetCode/1051-height-checker/1051-height-checker.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def heightChecker(self, heights):
+        expected = sorted(heights)
+        result = 0
+        for i in range(len(heights)):
+            if heights[i] != expected[i]:
+                result += 1
+        return result
+        
